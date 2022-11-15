6bea4f45b4c305907a2cc957745bce6464015c27
Ali Hassan Mirza <Ali.Hassan.Mirzaa@gmail.com>
2022-11-16 00:13:12 +0500

    changes

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-const UserRoute = require('./app/routes.js')
+const express = require('express')
+const app = express();
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+const UserRoute = require('./app/routes/User.js')
+
+
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Databse Connected Successfully!!");
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
 
 app.use('/user', UserRoute)
